af095ed61e2aeb611a174b406310782fc9bfd825
Brock Meyer <brockm87@gmail.com>
2016-06-05 14:48:55 -0500

    verified working again

---
+++ src/ConferenceApp/typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/angular-resource/index.d.ts" />
+/// <reference path="globals/angular-ui-bootstrap/index.d.ts" />
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment-node/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
